205ba624f0d02bcf5f5c544ce17ba6d033b1c291
Ujwal Komarla <ujwal_komarla@ncsu.edu>
2014-10-21 02:58:44 -0400

    Code with ConnectUDPsock(before changing char **serverIP to unsigned int **serverIP)

--- customFunctions.c
+++ customFunctions.c
@@ -16,5 +16,5 @@ int createUDPsock(void){
 	myAddr.sin_addr.s.addr = htonl(INADDR_ANY);
 	myAddr.sin_port = htons(0);//Let the OS chose a port on behalf of us.
 	if(bind(sockID,(struct sockaddr *)&myAddr, sizeof(myAddr))<0) DieWithError("Unable to do a bind on socket"); 
-	return 0;
+	return sockID;
 }
