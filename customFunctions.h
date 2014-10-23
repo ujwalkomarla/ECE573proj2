4cf0a256c753270a199ff3470d960133898233a3
Ujwal Komarla <ujwal_komarla@ncsu.edu>
2014-10-23 18:47:47 -0400

    Updated makefileClient to use thread programming

--- customFunctions.h
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-void DieWithError(char *);
-int createUDPsock(void);
-void makeSegment(unsigned int, unsigned short int, char *, unsigned int);
-unsigned short int segmentChecksum(unsigned int, unsigned short int, unsigned short int *, unsigned int);
-unsigned int sendSegment(unsigned int **, unsigned int, unsigned int , char *, unsigned int);
