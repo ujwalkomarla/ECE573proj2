929c0eb020e3052acf4b9b0809526cbac5a79d2a
Ujwal Komarla <ujwal_komarla@ncsu.edu>
2014-10-23 22:16:18 -0400

    Basic Client Thread to send segments

---
+++ ClientSenderThreadFunc.c
@@ -0,0 +1,36 @@
+#include"customDefinitions.h"
+
+
+
+void* ClientSenderThreadFunc(void *msg){	
+	struct threadArgument *rcvdArgument = (struct threadArgument *)msg;
+	unsigned int segmentSize = rcvdArgument->fileTransferInfo->segmentSize;
+	char *buf = (char *) malloc(segmentSize*sizeof(char));
+	char *fileNameToTransfer = rcvdArgument->fileTransferInfo->fileNameToTransfer;
+	FILE *fileToTransfer = fopen(fileNameToTransfer,"r");
+	if(NULL==fileToTransfer) DieWithError("Couldn't open the file to be transferred\r\n");
+	unsigned int seqNo = 0;
+	unsigned int MaxNoOfDataBytesInSegment = segmentSize*sizeof(char) - HEADERSIZE;
+	int fileReadSize;
+	while((fileReadSize = fread((buf + HEADERSIZE), sizeof(char), MaxNoOfDataBytesInSegment, fileToTransfer))>0){
+		
+		makeSegment(seqNo, SEGMENT_TYPE_DATA, buf, fileReadSize);
+		sendSegment(rcvdArgument->serverInfo,buf,(fileReadSize+HEADERSIZE));
+		
+		seqNo += segmentSize;
+	}
+}	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+/*
+Thread Parameters: (fileNameToTransfer, segmentSize), (struct sockaddr_in ClientServerConn, serverPortNumber(Doesn't that info be in sockaddr_in), numberOfServers)
+*/
