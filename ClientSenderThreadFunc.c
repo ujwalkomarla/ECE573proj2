086b9c8d4bfd388b5fcda59bf8e018b0f1dc1d5a
Ujwal Komarla <ujwal_komarla@ncsu.edu>
2014-10-25 19:44:22 -0400

    Updated sender thread to utilize on SEND_NEXT and serverACK variables

--- ClientSenderThreadFunc.c
+++ ClientSenderThreadFunc.c
@@ -3,21 +3,28 @@
 
 
 void* ClientSenderThreadFunc(void *msg){	
+	extern unsigned int SEND_NEXT;
+	unsigned int i;
 	struct threadArgument *rcvdArgument = (struct threadArgument *)msg;
 	unsigned int segmentSize = rcvdArgument->fileTransferInfo->segmentSize;
 	char *buf = (char *) malloc(segmentSize*sizeof(char));
 	char *fileNameToTransfer = rcvdArgument->fileTransferInfo->fileNameToTransfer;
 	FILE *fileToTransfer = fopen(fileNameToTransfer,"r");
 	if(NULL==fileToTransfer) DieWithError("Couldn't open the file to be transferred\r\n");
-	unsigned int seqNo = 0;
 	unsigned int MaxNoOfDataBytesInSegment = segmentSize*sizeof(char) - HEADERSIZE;
 	int fileReadSize;
 	while((fileReadSize = fread((buf + HEADERSIZE), sizeof(char), MaxNoOfDataBytesInSegment, fileToTransfer))>0){
+		while(!SEND_NEXT);
 		
-		makeSegment(seqNo, SEGMENT_TYPE_DATA, buf, fileReadSize);
+		//This instruction is required first, so that the older ACK's are not accepted
+		(rcvdArgument->fileTransferInfo->seqNo) += segmentSize;
+		for(i = 0;i<rcvdArgument->serverInfo->numberOfServers;i++){
+			rcvdArgument->serverInfo->serverACK[i] = 0;
+		}
+		makeSegment(rcvdArgument->fileTransferInfo->seqNo, SEGMENT_TYPE_DATA, buf, fileReadSize);
+		SEND_NEXT = 0;
 		sendSegment(SEND, rcvdArgument->serverInfo,buf,(fileReadSize+HEADERSIZE));
 		
-		seqNo += segmentSize;
 	}
 	return NULL;
 }	
