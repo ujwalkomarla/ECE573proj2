f7f184957d8425558f6009436a3f65421c2df39f
Ujwal Komarla <uskomarl@ncsu.edu>
2016-02-19 10:17:44 -0500

    Create AmazonAWS.sh

---
+++ AmazonAWS.sh
@@ -0,0 +1,6 @@
+cd ./Automate
+gnome-terminal -t S0 -e "ssh -i MyUbuntuAMIkeys.pem ubuntu@54.69.244.155"
+gnome-terminal -t S1 -e "ssh -i MyUbuntuAMIkeys.pem ubuntu@54.187.106.242"
+gnome-terminal -t S2 -e "ssh -i MyUbuntuAMIkeys.pem ubuntu@54.186.163.30"
+gnome-terminal -t S3 -e "ssh -i MyUbuntuAMIkeys.pem ubuntu@54.68.124.215"
+gnome-terminal -t S4 -e "ssh -i MyUbuntuAMIkeys.pem ubuntu@54.69.251.47"
